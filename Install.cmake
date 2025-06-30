708ae9fe72c28d1a76b065473f96cbdc57eb385f
Silverlan <silverlann@gmail.com>
2025-06-30 10:14:51 +0200

    build: update cmake script

--- Install.cmake
+++ Install.cmake
@@ -2,14 +2,4 @@ set(INSTALL_PATH "addons/virtual_reality/modules/openvr/")
 pr_install_create_directory("${INSTALL_PATH}")
 pr_install_targets(pr_openvr INSTALL_DIR "${INSTALL_PATH}")
 
-if(WIN32)
-    pr_install_files(
-        "${CMAKE_CURRENT_LIST_DIR}/third_party/openvr/bin/win64/openvr_api.dll"
-        INSTALL_DIR "${INSTALL_PATH}"
-    )
-else()
-    pr_install_files(
-        "${CMAKE_CURRENT_LIST_DIR}/third_party/openvr/bin/linux64/libopenvr_api.so"
-        INSTALL_DIR "${INSTALL_PATH}"
-    )
-endif()
+pr_install_binaries(openvr)
---
+++ cmake/modules/Findopenvr.cmake
@@ -0,0 +1,28 @@
+set(PCK "openvr")
+
+if (${PCK}_FOUND)
+  return()
+endif()
+
+find_path(${PCK}_INCLUDE_DIR
+  NAMES openvr.h
+  HINTS
+    ${CMAKE_CURRENT_SOURCE_DIR}/third_party/openvr/headers
+)
+
+find_library(${PCK}_LIBRARY
+  NAMES openvr_api
+  HINTS
+    ${CMAKE_CURRENT_SOURCE_DIR}/third_party/openvr/lib/linux64
+    ${CMAKE_CURRENT_SOURCE_DIR}/third_party/openvr/lib/win64
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(${PCK}
+  REQUIRED_VARS ${PCK}_LIBRARY ${PCK}_INCLUDE_DIR
+)
+
+if(${PCK}_FOUND)
+  set(${PCK}_LIBRARIES   ${${PCK}_LIBRARY})
+  set(${PCK}_INCLUDE_DIRS ${${PCK}_INCLUDE_DIR})
+endif()
